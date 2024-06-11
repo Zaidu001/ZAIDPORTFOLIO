8dfdeff12d57d8f46f6b157dc8486c963ab41aea
Zaidu001 <133227450+Zaidu001@users.noreply.github.com>
2024-06-11 15:51:53 +0530

    Add files via upload

---
+++ zportfolio/zaid.js
@@ -0,0 +1,10 @@
+// let links= document.querySelectorAll(".container a");
+// for(link of links){
+//     link.style.color="white"
+// }
+
+// for(let i=0; i<links.length; i++){
+//     links[i].style.color="purple"
+// }
+
+
